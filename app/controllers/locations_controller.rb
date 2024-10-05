1f228a626a7566e7bc90e31952e5262f07299835
mrmicrowaveoven <benj@zagorski.com>
2024-10-04 22:13:58 -0500

    Now sorts locations by id

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
   def index
     locations_array = Location.where(active: true).all
+    locations_array = locations_array.sort_by(&:id)
     locations_with_addresses = locations_array.map do |location|
       location.attributes.merge({addresses: location.addresses.map {|address| address.address_string}})
     end
