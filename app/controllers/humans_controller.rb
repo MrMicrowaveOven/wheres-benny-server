a64a9bad13f95df58bbf39ee8e7e55074294b2bc
mrmicrowaveoven <benj@zagorski.com>
2024-10-04 19:19:10 -0500

    Added Human Update endpoint, which will recieve updates to human locations

---
+++ app/controllers/humans_controller.rb
@@ -0,0 +1,17 @@
+class HumansController < ApplicationController
+    def update
+        name = human_params['name']
+        human = Human.find_by_name(name)
+        if human
+            human.update(lat: human_params['lat'], lng: human_params['lng'], location_last_updated: Time.now)
+        else
+            human = Human.create(name: human_params['name'], lat: human_params['lat'], lng: human_params['lng'], location_last_updated: Time.now)
+        end
+        render :json => human
+    end
+
+    private
+    def human_params
+      params.require(:human).permit(:name, :lat, :lng)
+    end
+end
