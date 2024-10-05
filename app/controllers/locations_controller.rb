59023f6d6c62fbe92ef7625008b14ca951bd20b5
mrmicrowaveoven <benj@zagorski.com>
2024-10-04 21:52:15 -0500

    Added Active to Locations table

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   def index
-    locations_array = Location.all
+    locations_array = Location.where(active: true).all
     locations_with_addresses = locations_array.map do |location|
       location.attributes.merge({addresses: location.addresses.map {|address| address.address_string}})
     end
