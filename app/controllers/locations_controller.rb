49ff930a829c3fa516b6b457bb987f5768d4b750
mrmicrowaveoven <benj@zagorski.com>
2024-10-05 16:13:50 -0500

    Now only returns humans who have checked in in the last 5 minutes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,7 @@ class LocationsController < ApplicationController
     locations_with_addresses = locations_array.map do |location|
       location.attributes.merge({
         addresses: location.addresses.map {|address| address.address_string},
-        humans: location.humans.map {|human| human.name}
+        humans: location.humans.where("location_last_updated > ?", 5.minutes.ago).map {|human| human.name}
       })
     end
     render :json => locations_with_addresses
